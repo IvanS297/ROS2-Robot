357035bd5b2a5d55a0a62fda79cb3c99a8eac68c
IvanS297 <ivan.ser202@gmail.com>
2026-05-11 10:49:45 +0700

    Добавил ROS2 Control

---
+++ src/view_robot/launch/launch_robot.launch.py
@@ -0,0 +1,113 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction, RegisterEventHandler
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.event import OnProcessStart
+from launch_ros.actions import Node
+from launch.substitutions import Command
+import xacro
+
+
+def generate_launch_description():
+
+    pkg_path = get_package_share_directory('view_robot')
+    
+    xacro_file = os.path.join(pkg_path, 'urdf', 'robot.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_description = {'robot_description': robot_description_config.toxml()}
+    sllidar_dir = get_package_share_directory('sllidar_ros2')
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        parameters=[]
+    )
+
+    sllidar_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(sllidar_dir, 'launch', 'sllidar_c1_launch.py')
+        ),
+        launch_arguments={
+            'serial_port': '/dev/ttyUSB0',
+            'frame_id': 'lidar_link',
+        }.items()
+    )
+
+    camera_node = Node(
+        package='v4l2_camera',
+        executable='v4l2_camera_node',
+        name='v4l2_camera',
+        parameters=[{
+            'image_size': [640, 480],
+            'camera_frame_id': 'camera_optical_frame',
+        }]
+    )
+
+    robot_description_command = Command(["ros2 param get --hide-type /robot_state_publisher robot_description"]) 
+    contorller_params_file = os.path.join(get_package_share_directory('view_robot'), 'config', 'my_controller.yaml')
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description_command}, contorller_params_file],
+    )
+
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"]
+    )
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"]
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
+
+    rviz_config = os.path.join(pkg_path, 'rviz', 'default.rviz')
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config] if os.path.exists(rviz_config) else [],
+        parameters=[],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='false'),
+        robot_state_publisher,
+        joint_state_publisher,
+        sllidar_launch,
+        camera_node,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner,
+        #rviz,
+    ])
--- src/view_robot/launch/sim.launch.py
+++ src/view_robot/launch/sim.launch.py
@@ -58,6 +58,7 @@ def generate_launch_description():
             '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
             '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
             '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
+            '/scan_gz@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
         ],
         output='screen'
     )
